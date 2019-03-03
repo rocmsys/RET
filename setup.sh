fb57f3239bc18859dfaea11063813d10b4616f3e
reger-men <adil-lashab@hotmail.com>
2019-03-03 03:00:29 +0100

    os function

---
+++ setup.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# Check OS
+# Get OS and Version
+# Return OS, Version and the Architecture
+function getOSInfo(){
+	# Init output variables 
+	local  __res_os=$1
+	local  __res_ver=$2
+	local  __res_arch=$3
+
+	if [ -f /etc/os-release ]; then
+		. /etc/os-release
+		local __OS=$NAME
+		local __VER=$VERSION_ID
+	elif [ -f /etc/lsb-release ]; then
+    		. /etc/lsb-release
+	        local __OS=$DISTRIB_ID
+		local __VER=$DISTRIB_RELEASE
+	else
+	    	local __OS=$(uname -s)
+		local __VER=$(uname -r)
+	fi
+
+	# Get OS Architecture
+	if [ $(uname -m) == 'x86_64' ]; then
+    		local __ARCH=x64
+	else
+		local __ARCH=x86
+	fi
+
+	eval $__res_os="'$__OS'"
+	eval $__res_ver="'$__VER'"
+	eval $__res_arch="'$__ARCH'"
+}
+
+function checkOs(){
+	if [[ "$OSTYPE" == "linux-gnu" ]]; then
+		getOSInfo OS VER ARCH
+		echo $OS $VER $ARCH
+	else
+		echo "OS not supported!"
+	fi
+}
+
+
+# Check OS
+echo -e Checking OS...
+checkOs
+
+
+# 1.Install rocm-kernel
+#sudo apt update
+#sudo apt install linux-headers-4.13.0-32-generic linux-image-4.13.0-32-generic linux-image-extra-4.13.0-32-generic linux-signed-image-4.13.0-32-generic
+
