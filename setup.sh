144b558921a04ccbb61a53587f0f5a4895e86802
reger-men <adil-lashab@hotmail.com>
2019-03-13 23:33:11 +0100

    YAML Parser and Array

--- setup.sh
+++ setup.sh
@@ -6,31 +6,36 @@ tput clear
 # Include files
 . src/definitions
 
+# read yaml file
+yaml2arr requirements/os_req.yml YAML_ARRAY
+echo ${YAML_ARRAY[3]}
+
+
+
 
 # Check OS
 function checkOs(){
-	if [[ "$OSTYPE" == "linux-gnu" ]]; then
-		getOSInfo OS VER KERNEL ARCH
-		logPrint "DET" "OS" "${OS}" "${SUCC}"
-		logPrint "DET" "VERSION" "${VER}" "${SUCC}"
-		logPrint "DET" "KERNEL" "${KERNEL}" "${SUCC}"
-		logPrint "DET" "ARCH" "${ARCH}" "${SUCC}"
-	else
-		logPrint "OS" "Not Supported!" "${FAIL}"
-	fi
+    if [[ "$OSTYPE" == "linux-gnu" ]]; then
+        getOSInfo OS VER KERNEL ARCH
+        logPrint "DET" "OS" "${OS}" "${SUCC}"
+	logPrint "DET" "VERSION" "${VER}" "${SUCC}"
+	logPrint "DET" "KERNEL" "${KERNEL}" "${SUCC}"
+	logPrint "DET" "ARCH" "${ARCH}" "${SUCC}"
+    else
+        logPrint "ERR" "OS Not Supported!" "$OSTYPE" "${FAIL}"
+    fi
 }
 
-checkDeps ${GLB_DEPS}
+# Check Dependencies
+getYAMLValue "dependencies|globale|name" VAl_ARRAY "${YAML_ARRAY[@]}" 
+echo "${VAl_ARRAY[2]}"
+#checkDeps ${GLB_DEPS}
+
 # Check OS
 logPrint "STEP" "Checking OS"
 checkOs
 
-# read yaml file
-#eval $(parse_yaml requirements/os_req.yml)
-parse_yaml requirements/os_req.yml
-
-# access yaml content
-echo "parse yaml"
-echo $config_development_database
-
 # 1.Install rocm-kernel
+
+
+
