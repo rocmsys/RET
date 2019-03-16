01ed2bae4719ef85610e9e3513e8ae72bcd522ca
reger-men <adil-lashab@hotmail.com>
2019-03-16 07:17:00 +0100

    check os from yaml

--- setup.sh
+++ setup.sh
@@ -7,48 +7,24 @@ tput clear
 . src/definitions
 
 # Check privileges
-if ! isSudo; then
-    if ! sudo -n true 2>/dev/null; then  # Check sudo for the current terminal session
-        logPrint "NOTE" "You need the root or sudo privileges to run this script." 
-        sudo echo 2>/dev/null 1>&2
-
-        EXIT_STATE=$?
-        if [ "$EXIT_STATE" -ne 0 ]; then
-            logPrint "ERR" "User has no root or sudo privileges!" "$USER" "${FAIL}"
-        fi
-    fi
+if ! isSudo; then 
+        logPrint "ERR" "Please run the following command instead!" "sudo $0" "${FAIL}"
 fi
 
 # read yaml file
 yaml2arr "requirements/req.yml" YAML_ARRAY
-
-
-# Check OS
-function checkOs {
-    if [[ "$OSTYPE" == "linux-gnu" ]]; then
-        getOSInfo OS VER KERNEL ARCH
-        logPrint "DET" "OS" "${OS}" "${SUCC}"
-	logPrint "DET" "VERSION" "${VER}" "${SUCC}"
-	logPrint "DET" "KERNEL" "${KERNEL}" "${SUCC}"
-	logPrint "DET" "ARCH" "${ARCH}" "${SUCC}"
-    else
-        logPrint "ERR" "OS Not Supported!" "$OSTYPE" "${FAIL}"
-    fi
-}
-
+#echo "${YAML_ARRAY[@]}"
 
 # Check OS
 logPrint "STEP" "Checking OS"
-checkOs
+checkOs 
 
 
 # Check Dependencies
-if getYAMLValue "dependencies|globale|name" DEPS_ARRAY YAML_ARRAY; then 
-    getYAMLValue "dependencies|globale|name|version" VER_ARRAY YAML_ARRAY
-    checkDeps DEPS_ARRAY VER_ARRAY
-else 
-    logPrint "ERR" "No Dependencies Found with this key!" "dependencies|globale|name" "${FAIL}"
-fi
+getYAMLValue "dependencies|globale|name" YAML_ARRAY DEPS_ARRAY
+getYAMLValue "dependencies|globale|name|version" YAML_ARRAY VER_ARRAY
+checkDeps DEPS_ARRAY VER_ARRAY
+
 
 # 1.Install rocm-kernel
 
