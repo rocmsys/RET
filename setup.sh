994f7960978c84c3c359b0fda6dfb2621660f913
reger-men <adil-lashab@hotmail.com>
2019-03-21 01:59:31 +0100

    kernel update

--- setup.sh
+++ setup.sh
@@ -6,6 +6,9 @@ tput clear
 # Include files
 . src/definitions
 
+# Trap ctr-c
+trap exitAll SIGINT
+
 # Check privileges
 if ! isSudo; then 
         logPrint "ERR" "Please run the following command instead!" "sudo $0" "${FAIL}"
@@ -24,9 +27,9 @@ checkOs
 
 
 # Check Dependencies
-getYAMLValue "dependencies|globale|name" YAML_ARRAY DEPS_ARRAY
-getYAMLValue "dependencies|globale|name|version" YAML_ARRAY VER_ARRAY
-checkDeps DEPS_ARRAY VER_ARRAY
+#getYAMLValue "dependencies|globale|name" YAML_ARRAY DEPS_ARRAY
+#getYAMLValue "dependencies|globale|name|version" YAML_ARRAY VER_ARRAY
+#checkDeps DEPS_ARRAY VER_ARRAY
 
 
 # 1.Install rocm-kernel
